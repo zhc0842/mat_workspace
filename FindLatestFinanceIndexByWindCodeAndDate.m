5f03b3bad94c3857473d8fda9c1ba77472d9ee79
zhc0842 <zhc0842@163.com>
2015-02-03 17:45:55 +0800

    2015年2月3日17:45:41

--- FindLatestFinanceIndexByWindCodeAndDate.m
+++ FindLatestFinanceIndexByWindCodeAndDate.m
@@ -2,42 +2,64 @@ function rtnFinanceData = FindLatestFinanceIndexByWindCodeAndDate(date_,code_,w_
     if nargin < 4,
         windField_ = 'eps_basic,bps,orps_ttm,surpluscapitalps,surplusreserveps';
     end
-    windField = ['stm_issuingdate,ipo_date',windField_];
+    windField = ['stm_issuingdate,',windField_];
     %ipo_date是否需要检查？
     %stm_predict_issingdate 预估财报发布日期
     tradeCode = GetTradeCode(code_);
+    
     %%
     %获取指定交易日中证800指数包含的所有股票代码 data的第二列
     %[w_wset_data,w_wset_codes,w_wset_fields,w_wset_times,w_wset_errorid,w_wset_reqid]=w.wset('IndexConstituent','date=20150203;windcode=000906.SH');
     %tradeCode800 = w_wset_data(2);
     %%
 %   disp(tradeCode); 
+
     rptDate = GetLatestRptDate(date_);
     disp(rptDate);
     [financeData,~,~,~,errorid,~] = w_.wss(tradeCode,windField,['rptDate=',rptDate]);
     assert(errorid == 0, '#Error:FindLatestFinanceIndexByWindCodeAndDate:wind error.');
-    disp(financeData);
     rowNum = size(financeData,1);
     nDate = datenum(num2str(date_),'yyyymmdd');
     for i = 1 : rowNum
         rowData = financeData(i,:);
-        stm_issuingdate = rowData(1);
+        stm_issuingdate = rowData{1};
         nStm_issuingdate = 0;
         try
             nStm_issuingdate = datenum(stm_issuingdate,'yyyy/mm/dd');
         catch
-            disp(['#Error:FindLatestFinanceIndexByWindCodeAndDate:',tradeCode{i},': ',stm_issuingdate,' no valid stm_issuingdate,ipo_date maybe later than it.']);
+            disp('#Error:FindLatestFinanceIndexByWindCodeAndDate:invalid stm_issuingdate,ipo_date maybe later than it.');
             continue;
         end
         if nStm_issuingdate <= nDate ,
             %insert the rowdata into db MultiFactors800
-            %
+            conn = database('DB','user','pwd','com.microsoft.sqlserver.jdbc.SQLServerDriver','jdbc:sqlserver://127.0.0.1:1433;databaseName=DB');
+            if isconnection(conn) == 1 ,%若返回1则认为连接成功
+                exdata = {'San Diego', 88;'Smith Jhon',99};%插入一个数组，可以考虑插入之前一直可用的数据，提前设置一个flag
+                colnames = {'City', 'Avg_Temp'};
+                fastinsert(conn, 'Temperatures', colnames, exdata);
+            end
+            close(conn);
+     
         else
             %this rowdata is invalid which should not be seen on that date_
+            %so use the previous rptDate to get the latest Financedata
             preRptDate = GetLatestRptDate(rptDate);
             [preFinanceData,~,~,~,preErrorid,~] = w_.wss(tradeCode{i},windField,['rptDate=',preRptDate]);
             assert(preErrorid == 0, '#Error:FindLatestFinanceIndexByWindCodeAndDate:wind error.');
-            disp(preFinanceData);
+            preRowData = preFinanceData(1,:);
+            pre_stm_issuingdate = preRowData{1};
+            try
+                preNStm_issuingdate = datenum(pre_stm_issuingdate,'yyyy/mm/dd');
+            catch
+                disp('#Error:FindLatestFinanceIndexByWindCodeAndDate:invalid stm_issuingdate,ipo_date maybe later than it.');
+                continue;
+            end
+            if preNStm_issuingdate <= nDate ,
+                %insert the rowdata into db MultiFactors800
+                %
+            else
+                
+            end
         end
         
         
@@ -45,7 +67,16 @@ function rtnFinanceData = FindLatestFinanceIndexByWindCodeAndDate(date_,code_,w_
     
     
 end
-    
+
+function flag = IsValidIssuingDate(stm_issuingdate,rptDate)
+%校验实际财报发布日期是否 在截止日期前发布
+% 一季报0331：4月30日前
+% 半年报0630：8月31日前
+% 三季报0930：10月31日前
+% 年报  1231：次年4月30日前
+
+end
+
 function rptDate = GetLatestRptDate(date_)
         date_ = num2str(date_);
         y_ = str2num(date_(1:4));
